22bdceb3db3aece6a625b3f7d8b978789e6ddda2
fjkrtrtr <abuelametraumolaabuela@outlook.com>
2022-11-01 21:22:19 -0500

    Sample Commit

---
+++ UserControllogin.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ReturnValuesFromUserControl
+{
+    partial class UserControllogin
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxPassword = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.groupBoxPassword.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxPassword
+            // 
+            this.groupBoxPassword.Controls.Add(this.textBox2);
+            this.groupBoxPassword.Controls.Add(this.label3);
+            this.groupBoxPassword.Controls.Add(this.label2);
+            this.groupBoxPassword.Controls.Add(this.textBoxUsername);
+            this.groupBoxPassword.Controls.Add(this.label1);
+            this.groupBoxPassword.Location = new System.Drawing.Point(14, 17);
+            this.groupBoxPassword.Name = "groupBoxPassword";
+            this.groupBoxPassword.Size = new System.Drawing.Size(338, 82);
+            this.groupBoxPassword.TabIndex = 0;
+            this.groupBoxPassword.TabStop = false;
+            this.groupBoxPassword.Text = "Login";
+            this.groupBoxPassword.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(76, 19);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(177, 20);
+            this.textBoxUsername.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(76, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(177, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password";
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(90, 88);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 5;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            // 
+            // UserControllogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.groupBoxPassword);
+            this.Name = "UserControllogin";
+            this.Size = new System.Drawing.Size(299, 118);
+            this.groupBoxPassword.ResumeLayout(false);
+            this.groupBoxPassword.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonLogin;
+    }
+}
---
+++ UserControllogin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReturnValuesFromUserControl
+{
+    public partial class UserControllogin : UserControl
+    {
+        public UserControllogin()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+            var username = textboxUsername.Text.Trim();
+            var password = textboxPassword.Text.Trim(); 
+            if(username.Equals("abc ") && password.Equals("def"))
+            {
+                MessageBox.Show("Success");
+            } 
+            else
+            {
+                MessageBox.Show("Failed");
+            }
+        }
+    }
+}
